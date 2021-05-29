c9fbd36cf4ed91a095330c9f745a73c6bb05b270
sebas <sebas@LAPTOP-PALU9LV1>
2021-05-28 21:30:11 -0300

    task : tp 8

---
+++ GrupoSGCR/src/main/java/ar/edu/unju/fi/tp8/model/Compra.java
@@ -0,0 +1,152 @@
+package ar.edu.unju.fi.tp8.model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+@Table(name = "COMPRAS")
+public class Compra {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "com_cantidad")
+	private int cantidad;
+	
+	@Column(name = "com_total")
+	private double total;
+	
+	@Autowired
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "pro_id")
+	private Producto producto;
+	
+	@OneToMany(mappedBy = "compra")
+	private List<Producto> productos = new ArrayList<Producto>();
+	
+	
+	
+	@Autowired
+	@OneToOne
+	@JoinColumn(name = "cli_id")
+	private Cliente cliente;
+	
+	
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+
+	public Compra() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	public Compra(Long id, int cantidad, double total, Producto producto, List<Producto> productos, Cliente cliente) {
+		super();
+		this.id = id;
+		this.cantidad = cantidad;
+		this.total = total;
+		this.producto = producto;
+		this.productos = productos;
+		this.cliente = cliente;
+	}
+
+
+	/***
+	 * 
+	public Compra(Long id, int cantidad, double total, Producto producto, List<Producto> productos) {
+		super();
+		this.id = id;
+		this.cantidad = cantidad;
+		this.total = total;
+		this.producto = producto;
+		this.productos = productos;
+	}
+	 * 
+	 */
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Compra [id=" + id + ", cantidad=" + cantidad + ", total=" + total + ", producto=" + producto
+				+ ", productos=" + productos + ", cliente=" + cliente + "]";
+	}
+	
+	
+
+	/***
+	@Override
+	public String toString() {
+		return "Compra [id=" + id + ", cantidad=" + cantidad + ", total=" + total + ", producto=" + producto
+				+ ", productos=" + productos + "]";
+	}
+	 * 
+	 */
+
+	
+}
---
+++ GrupoSGCR/src/main/java/ar/edu/unju/fi/tp8/model/Producto.java
@@ -0,0 +1,128 @@
+package ar.edu.unju.fi.tp8.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+@Table(name = "PRODUCTOS")
+public class Producto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "prod_codigo", nullable = false)
+	private long codigo;
+	
+	@Column(name = "prod_nombre", length = 100)
+	private String nombre;
+	
+	@Column(name = "prod_precio")
+	private double precio;
+	
+	@Column(name = "prod_marca", length = 100)
+	private String marca;
+	
+	@Column(name = "prod_stock")
+	private int stock;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name = "com_id")
+	private Compra compra;
+	
+	// Constructor empty
+	public Producto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Producto(Long id, int codigo, String nombre, double precio, String marca, int stock, Compra compra) {
+		super();
+		this.id = id;
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.marca = marca;
+		this.stock = stock;
+		this.compra = compra;
+	}
+
+	// Setters and getters
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Compra getCompra() {
+		return compra;
+	}
+
+	public void setCompra(Compra compra) {
+		this.compra = compra;
+	}
+
+	
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	// to String
+
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", codigo=" + codigo + ", nombre=" + nombre + ", precio=" + precio + ", marca="
+				+ marca + ", stock=" + stock + ", compra=" + compra + "]";
+	}
+	
+	
+	
+}
